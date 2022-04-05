97b86964f9401e2db942c138db13b5d90b1b1062
Willi-mue <98591138+Willi-mue@users.noreply.github.com>
2022-04-05 22:47:01 +0200

    update all

---
+++ app.py
@@ -0,0 +1,210 @@
+import sys
+import ctypes
+
+from logic import *
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+
+
+class main_application(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowIcon(QIcon("bilder/icon.png"))
+        # Setzen des Icons
+        my_app_id = 'by_Müller_Willi.Verkehrs_Simulation.1.0'
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(my_app_id)
+
+        self.setWindowTitle('Verkehrs_Simulation')
+
+        self.hoehe = 1080
+        self.breite = 1920
+
+        self.resize(self.breite, self.hoehe)
+        self.setMinimumSize(self.breite, self.hoehe)
+
+        self.sim = QLabel(self)
+        self.figures = QLabel(self)
+
+        self.show_line = False
+
+        self.counter = 0
+        self.stop = False
+        self.tile_size = 20
+        self.ampel_tick = True
+        self.cars = {}
+        self.ampel_cords = []
+        self.ampel_tick_rate = 32
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.animation)
+        self.speed = 100
+        self.timer.start(self.speed)
+
+        self.setWindowTitle(str(self.speed))
+
+        self.links_oben = QImage("bilder/links_oben.png")
+        self.rechts_oben = QImage("bilder/rechts_oben.png")
+        self.links_unten = QImage("bilder/links_unten.png")
+        self.rechts_unten = QImage("bilder/rechts_unten.png")
+        self.horizontal = QImage("bilder/horizontal.png")
+        self.vertikal = QImage("bilder/vertikal.png")
+
+        self.ampel1 = QImage("bilder/ampel_1.png")
+        self.ampel2 = QImage("bilder/ampel_2.png")
+
+        self.up = QImage("bilder/up.png")
+        self.down = QImage("bilder/down.png")
+        self.left = QImage("bilder/left.png")
+        self.right = QImage("bilder/right.png")
+        self.point = QImage("bilder/point.png")
+
+        self.first_draw()
+        make_cars(self.cars, anzahl=80, right_cars=25)
+
+    def first_draw(self):
+        canvas = QPixmap(self.breite, self.hoehe)
+        canvas.fill(Qt.black)
+        self.sim.setPixmap(canvas)
+        self.sim.setGeometry(0, 0, self.breite, self.hoehe)
+
+        pen = QPen()
+        pen.setWidth(2)
+        pen.setColor(Qt.black)
+
+        painter_sim = QPainter(self.sim.pixmap())
+
+        for i in range(len(karte)):
+            for j in range(len(karte[i])):
+                if karte[i][j] == "-":
+                    painter_sim.drawImage(QRect((j * self.tile_size), (i * self.tile_size),
+                                                self.tile_size, self.tile_size), self.horizontal)
+                elif karte[i][j] == "|":
+                    painter_sim.drawImage(QRect((j * self.tile_size), (i * self.tile_size),
+                                                self.tile_size, self.tile_size), self.vertikal)
+                elif karte[i][j] == "T":
+                    self.ampel_cords.append([i, j])
+
+                elif karte[i][j] == "a":
+                    painter_sim.drawImage(QRect((j * self.tile_size), (i * self.tile_size),
+                                                self.tile_size, self.tile_size), self.links_oben)
+                elif karte[i][j] == "b":
+                    painter_sim.drawImage(QRect((j * self.tile_size), (i * self.tile_size),
+                                                self.tile_size, self.tile_size), self.rechts_oben)
+                elif karte[i][j] == "c":
+                    painter_sim.drawImage(QRect(j * self.tile_size, i * self.tile_size,
+                                                self.tile_size, self.tile_size), self.links_unten)
+                elif karte[i][j] == "d":
+                    painter_sim.drawImage(QRect(j * self.tile_size, i * self.tile_size,
+                                                self.tile_size, self.tile_size), self.rechts_unten)
+
+                if self.show_line:
+                    if karte[i][j] == ">":
+                        painter_sim.drawImage(QRect(j * self.tile_size, i * self.tile_size,
+                                                    self.tile_size, self.tile_size), self.right)
+                    elif karte[i][j] == "<":
+                        painter_sim.drawImage(QRect(j * self.tile_size, i * self.tile_size,
+                                                    self.tile_size, self.tile_size), self.left)
+                    elif karte[i][j] == "v":
+                        painter_sim.drawImage(QRect(j * self.tile_size, i * self.tile_size,
+                                                    self.tile_size, self.tile_size), self.down)
+                    elif karte[i][j] == "^":
+                        painter_sim.drawImage(QRect(j * self.tile_size, i * self.tile_size,
+                                                    self.tile_size, self.tile_size), self.up)
+                    elif karte[i][j] == ".":
+                        painter_sim.drawImage(QRect(j * self.tile_size, i * self.tile_size,
+                                                    self.tile_size, self.tile_size), self.point)
+
+    def animation(self):
+
+        self.setWindowTitle(str(self.speed))
+
+        if self.counter % self.ampel_tick_rate == 0 or self.counter == 0:
+            if self.ampel_tick:
+                self.ampel_tick = False
+            else:
+                self.ampel_tick = True
+
+        self.counter += 1
+
+        for key in self.cars:
+            self.cars[key].look_under()
+            self.cars[key].drive(key, self.cars, self.ampel_tick)
+
+        self.update()
+
+    def paintEvent(self, event):
+        self.setWindowTitle(str(self.speed))
+        canvas = QPixmap(self.breite, self.hoehe)
+        canvas.fill(QColor(0, 0, 0, 0))
+        self.figures.setPixmap(canvas)
+        self.figures.setGeometry(0, 0, self.breite, self.hoehe)
+
+        painter_figures = QPainter(self.figures.pixmap())
+
+        if self.ampel_tick:
+            for i in self.ampel_cords:
+                painter_figures.drawImage(QRect((i[1] * self.tile_size), (i[0] * self.tile_size),
+                                                self.tile_size, self.tile_size), self.ampel1)
+        else:
+            for i in self.ampel_cords:
+                painter_figures.drawImage(QRect((i[1] * self.tile_size), (i[0] * self.tile_size),
+                                                self.tile_size, self.tile_size), self.ampel2)
+
+        for key in self.cars:
+            painter_figures.fillRect(QRect(self.cars[key].pos_x * self.tile_size + self.tile_size // 4,
+                                           self.cars[key].pos_y * self.tile_size + self.tile_size // 4,
+                                           self.tile_size // 2, self.tile_size // 2),
+                                     QColor(self.cars[key].colour[0], self.cars[key].colour[1],
+                                            self.cars[key].colour[2]))
+
+    def keyPressEvent(self, event):
+        key = event.key()
+
+        if key == Qt.Key_P:
+            if not self.stop:
+                self.timer.stop()
+                self.stop = True
+            else:
+                self.stop = False
+                self.timer.start(self.speed)
+
+        if key == Qt.Key_E:
+            self.speed += 1
+            if not self.stop:
+                self.timer.start(self.speed)
+            self.update()
+
+        if key == Qt.Key_W:
+            if self.speed - 1 >= 0:
+                self.speed -= 1
+            if not self.stop:
+                self.timer.start(self.speed)
+            self.update()
+
+        if key == Qt.Key_Q:
+            if not self.show_line:
+                self.show_line = True
+                self.first_draw()
+                self.update()
+            else:
+                self.show_line = False
+                self.first_draw()
+                self.update()
+
+        if key == Qt.Key_R:
+            make_cars(self.cars, anzahl=80, right_cars=25)
+
+        if key == Qt.Key_Escape:
+            sys.exit()
+
+
+def main():
+    app = QApplication(sys.argv)
+    application = main_application()
+    application.show()
+    sys.exit(app.exec_())
+
+
+main()
---
+++ logic.py
@@ -0,0 +1,203 @@
+import random
+
+
+def read_map():
+    file = open('map.txt', 'r')
+    Lines = file.readlines()
+    return Lines
+
+
+def slow(effekt=80):
+    n = random.randint(0, effekt)
+    if n == effekt//2:
+        return False
+    return True
+
+
+def make_cars(autos: dict, anzahl=10, right_cars=0):
+    for i in range(anzahl):
+        if right_cars > 0:
+            autos[str(i)] = auto(colour=[0, 150, 0])
+            right_cars -= 1
+            autos[str(i)].right()
+
+        else:
+            autos[str(i)] = auto(colour=[150, 0, 0])
+
+        autos[str(i)].spawn()
+
+
+def check_ampel(direction, x, y, setting):
+    # True =  oben unten || False = links rechts
+    if direction == "up" and karte[y - 2][x - 2] == "T" and setting:
+        return False
+    elif direction == "down" and karte[y + 2][x + 2] == "T" and setting:
+        return False
+    elif direction == "left" and karte[y + 1][x - 4] == "T" and not setting:
+        return False
+    elif direction == "right" and karte[y - 1][x + 4] == "T" and not setting:
+        return False
+
+    return True
+
+
+def check_car_front(aktueller_key, autos: dict):
+    for key in autos:
+        if aktueller_key == key:
+            pass
+        else:
+            if autos[aktueller_key].direction == "left":
+                if autos[aktueller_key].pos_x - 1 == autos[key].pos_x and \
+                        autos[aktueller_key].pos_y == autos[key].pos_y:
+                    return False
+
+            elif autos[aktueller_key].direction == "right":
+                if autos[aktueller_key].pos_x + 1 == autos[key].pos_x and \
+                        autos[aktueller_key].pos_y == autos[key].pos_y:
+                    return False
+
+            elif autos[aktueller_key].direction == "up":
+                if autos[aktueller_key].pos_x == autos[key].pos_x and \
+                        autos[aktueller_key].pos_y - 1 == autos[key].pos_y:
+                    return False
+
+            elif autos[aktueller_key].direction == "down":
+                if autos[aktueller_key].pos_x == autos[key].pos_x and \
+                        autos[aktueller_key].pos_y + 1 == autos[key].pos_y:
+                    return False
+
+    return True
+
+
+karte = read_map()
+
+
+def abbiegen_random(x, y, direction):
+    check = []
+    if direction == "up":
+        if karte[y][x + 1] == ">":
+            check.append("right")
+        if karte[y - 1][x] == "^":
+            check.append("up")
+        if karte[y][x - 1] == "<":
+            check.append("left")
+    elif direction == "down":
+        if karte[y][x - 1] == "<":
+            check.append("left")
+        if karte[y + 1][x] == "v":
+            check.append("down")
+        if karte[y][x + 1] == ">":
+            check.append("right")
+    elif direction == "left":
+        if karte[y - 1][x] == "^":
+            check.append("up")
+        if karte[y][x - 1] == "<":
+            check.append("left")
+        if karte[y + 1][x] == "v":
+            check.append("down")
+    elif direction == "right":
+        if karte[y + 1][x] == "v":
+            check.append("down")
+        if karte[y][x + 1] == ">":
+            check.append("right")
+        if karte[y - 1][x] == "^":
+            check.append("up")
+
+    rand = random.randint(0, len(check) - 1)
+
+    return check[rand]
+
+
+def abbiegen_immer_rechts(x, y, direction):
+    if direction == "up":
+        if karte[y][x + 1] == ">":
+            return "right"
+        elif karte[y - 1][x] == "^":
+            return "up"
+        elif karte[y][x - 1] == "<":
+            return "left"
+    elif direction == "down":
+        if karte[y][x - 1] == "<":
+            return "left"
+        elif karte[y + 1][x] == "v":
+            return "down"
+        elif karte[y][x + 1] == ">":
+            return "right"
+    elif direction == "left":
+        if karte[y - 1][x] == "^":
+            return "up"
+        elif karte[y][x - 1] == "<":
+            return "left"
+        elif karte[y + 1][x] == "v":
+            return "down"
+    elif direction == "right":
+        if karte[y + 1][x] == "v":
+            return "down"
+        elif karte[y][x + 1] == ">":
+            return "right"
+        elif karte[y - 1][x] == "^":
+            return "up"
+
+
+class auto:
+    def __init__(self, colour):
+        self.direction = None
+        self.pos_x = None
+        self.pos_y = None
+        self.map = karte
+        self.right_car = False
+        self.colour = colour
+
+    def drive(self, key, autos: dict, ampel_setting):
+        test1 = check_car_front(key, autos)
+        test2 = check_ampel(self.direction, self.pos_x, self.pos_y, ampel_setting)
+        test3 = slow()
+        if test1 and test2 and test3:
+            if self.direction == "left":
+                self.pos_x -= 1
+            elif self.direction == "right":
+                self.pos_x += 1
+            elif self.direction == "up":
+                self.pos_y -= 1
+            elif self.direction == "down":
+                self.pos_y += 1
+
+    def right(self):
+        self.right_car = True
+
+    def look_under(self):
+        if self.map[self.pos_y][self.pos_x] == ">":
+            direction = "right"
+        elif self.map[self.pos_y][self.pos_x] == "<":
+            direction = "left"
+        elif self.map[self.pos_y][self.pos_x] == "^":
+            direction = "up"
+        elif self.map[self.pos_y][self.pos_x] == "v":
+            direction = "down"
+        elif self.map[self.pos_y][self.pos_x] == ".":
+            if self.right_car:
+                direction = abbiegen_immer_rechts(self.pos_x, self.pos_y, self.direction)
+            else:
+                direction = abbiegen_random(self.pos_x, self.pos_y, self.direction)
+        else:
+            direction = None
+            print("Error in look_under")
+
+        self.direction = direction
+
+    def spawn(self, x=None, y=None):
+        while not self.check_tile(x, y):
+            x = random.randint(0, len(self.map[0]) - 2)
+            y = random.randint(0, len(self.map) - 1)
+
+        self.pos_x = x
+        self.pos_y = y
+        self.look_under()
+
+    def check_tile(self, x, y):
+        if x is None and y is None:
+            return False
+        if self.map[y][x] == ">" or self.map[y][x] == "v" or self.map[y][x] == "^" or self.map[y][x] == "<":
+            return True
+        else:
+            return False
